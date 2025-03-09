2589c76e760900769bfca7b6748a0afd85328a44
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-03-09 09:56:20 -0400

    first commit

---
+++ constant/constant.ts
@@ -0,0 +1,37 @@
+export const navLinks = [
+  {
+    id: 1,
+    url: "#",
+    label: "Home",
+  },
+  {
+    id: 2,
+    url: "#",
+    label: "About",
+  },
+  {
+    id: 3,
+    url: "#",
+    label: "Work",
+  },
+  {
+    id: 4,
+    url: "#",
+    label: "Drilling",
+  },
+  {
+    id: 5,
+    url: "#",
+    label: "Help",
+  },
+  {
+    id: 6,
+    url: "#",
+    label: "Donate",
+  },
+  {
+    id: 7,
+    url: "#",
+    label: "Contact",
+  },
+];
