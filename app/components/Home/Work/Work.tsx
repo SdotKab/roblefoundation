6a2b62d9017982950202926f36b041c92304c51e
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-04-03 10:14:10 -0400

    delete image carousel helpers

--- app/components/Home/Work/Work.tsx
+++ app/components/Home/Work/Work.tsx
@@ -3,8 +3,6 @@
 import { motion } from 'framer-motion';
 
 import H2Text from '../../Helper/H2Text';
-import MyCarousel from '../../Helper/MyCarousel';
-import MyCarousel from '../../Helper/MyCarousel';
 import { WorkType } from '@/types/types';
 
 const works: Array<WorkType> = [
@@ -69,43 +67,6 @@ const Work = (props: Props) => {
         </div>
       </motion.div>
 
-      <BootstrapCarousel />
-      
-      {/* <div id="default-carousel" className="relative w-2/5" data-carousel="slide">
-        <div className="relative h-96 rounded-lg lg:h-96">
-          {works.map((item: WorkType, index) => (
-            <MyCarousel 
-              key={`${item.name}-${index}`}
-              name={item.name}
-              description={item.description}
-              image={item.image}
-            />
-          ))}
-          <div className="duration-700 ease-in-out" data-carousel-item>
-              <img src="/4Sincaro.jpg" className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-          </div>
-
-        </div>
-        
-        <button type="button" className="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 1 1 5l4 4"/>
-                    </svg>
-                <span className="sr-only"> Previousspan </span>
-              </span>
-        </button>
-
-        <button type="button" className="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-          <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg className="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4"/>
-                </svg>
-            <span className="sr-only">Nextspan</span>
-          </span>
-        </button>
-        
-      </div> */}
     </section>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,35 +2,84 @@ import type { Config } from "tailwindcss";
 
 
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    flowbite.content(),
   ],
   theme: {
-    extend: {
-      colors: {
-        "gray-20": "#FFFFFF",
-        "gray-50": "#dfebf0",
-        "gray-100": "#ccdbdf",
-        "gray-500": "#00475e",
-        "primary-100": "#e0f7ff",
-        "primary-300": "#a3e2ff",
-        "primary-500": "#66bfff",
-        "secondary-400": "#9c641f",
-        "secondary-500": "#ffc85b",
-      },
-      backgroundImage: (theme) => ({
-        "gradient-yellowred":
-          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",
-      }),
-      fontFamily: {
-        dmsans: ["DM Sans", "sans-serif"],
-        montserrat: ["Montserrat", "sans-serif"],
-      },
-    },  
+  	extend: {
+  		colors: {
+  			'gray-20': '#FFFFFF',
+  			'gray-50': '#dfebf0',
+  			'gray-100': '#ccdbdf',
+  			'gray-500': '#00475e',
+  			'primary-100': '#e0f7ff',
+  			'primary-300': '#a3e2ff',
+  			'primary-500': '#66bfff',
+  			'secondary-400': '#9c641f',
+  			'secondary-500': '#ffc85b',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		backgroundImage: '(theme) => ({\n        "gradient-yellowred":\n          "linear-gradient(90deg, #FF616A 0%, #FFC837 100%)",\n      })',
+  		fontFamily: {
+  			dmsans: [
+  				'DM Sans',
+  				'sans-serif'
+  			],
+  			montserrat: [
+  				'Montserrat',
+  				'sans-serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
   plugins: [
-  ],
+      require("tailwindcss-animate")
+],
 } satisfies Config;
