85f3d1b15fd5862cae311f3fbca954645c550fb2
Sama-E <112719147+Sama-E@users.noreply.github.com>
2025-04-04 09:34:53 -0400

    cleanup4

--- app/components/Home/Contact/Contact.tsx
+++ app/components/Home/Contact/Contact.tsx
@@ -6,9 +6,7 @@ import image10 from "@/public/image10.jpg";
 import H2Text from "../../Helper/H2Text";
 import Image from "next/image";
 
-
 const Contact = () => {
-
     const inputStyles = `mb-5 w-full rounded-lg border-2 border-gray-100 px-5 py-3 placeholder-black`;
 
     const {
@@ -17,7 +15,8 @@ const Contact = () => {
         formState: { errors }
     } = useForm();
     
-    const onSubmit = async(e:any) => {
+    // Change from `any` to `React.FormEvent<HTMLFormElement>`
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         const isValid = await trigger();
         if (!isValid) {
             e.preventDefault();
@@ -27,7 +26,7 @@ const Contact = () => {
     return (
         <section id="contact" className="mx-auto w-5/6 pt-24 pb-32 mt-10">
             <motion.div>
-            {/* HEADER */}
+                {/* HEADER */}
                 <motion.div 
                     className="md:w-3/5"
                     initial="hidden"
@@ -44,105 +43,99 @@ const Contact = () => {
                     </H2Text>
                 </motion.div>
                 {/* FORM AND IMAGE */}
-                    <div className="mt-5 justify-between gap-8 md:flex">
-                        <motion.div
-                            className="mt-10 basis-1/2 md:mt-0"
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                            transition={{ duration: 0.5 }}
-                            variants={{
-                                hidden: { opacity: 0, y: 50 },
-                                visible: { opacity: 1, y: 0 },
-                            }}
+                <div className="mt-5 justify-between gap-8 md:flex">
+                    <motion.div
+                        className="mt-10 basis-1/2 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, y: 0 },
+                        }}
+                    >
+                        <p className="my-5">
+                        <b>Make a Difference Today:</b> Every dollar, every effort, and every voice matters in the fight for clean water access. Contact us to learn more.
+                        </p>
+                        <form
+                            target="_blank"
+                            onSubmit={onSubmit}
+                            action="https://formsubmit.co/myemail.com"
+                            method="POST"
                         >
-                            <p className="my-5">
-                            <b>Make a Difference Today:</b> Every dollar, every effort, and every voice matters in the fight for clean water access. Contact us to learn more.
-                            </p>
-                            <form
-                                target="_blank"
-                                onSubmit={onSubmit}
-                                action="https://formsubmit.co/myemail.com"
-                                method="POST"
+                            <input 
+                                className={inputStyles}
+                                type="text"
+                                placeholder="NAME"
+                                {...register("name", {
+                                    required:true,
+                                    maxLength:100,
+                                })}
+                            />
+                            {errors.name && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.name.type === "required" && "This field is required."}
+                                    {errors.name.type === "maxLength" && "Max length is 100 characters."}
+                                </p>
+                            )}
+                            <input 
+                                className={inputStyles}
+                                type="text"
+                                placeholder="EMAIL"
+                                {...register("email", {
+                                    required:true,
+                                    pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                })}
+                            />
+                            {errors.email && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.email.type === "required" && "This field is required."}
+                                    {errors.email.type === "email" && "Invalid email address."}
+                                </p>
+                            )}
+                            <textarea 
+                                className={inputStyles}
+                                rows={4}
+                                cols={50}
+                                placeholder="MESSAGE"
+                                {...register("message", {
+                                    required:true,
+                                    maxLength:2000,
+                                })}
+                            />
+                            {errors.message && (
+                                <p className="mt-1 text-primary-500">
+                                    {errors.message.type === "required" && "This field is required."}
+                                    {errors.message.type === "maxLength" && "Max length is 2000 characters."}
+                                </p>
+                            )}
+                            <button
+                                type="submit"
+                                className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
                             >
-                                <input 
-                                    className={inputStyles}
-                                    type="text"
-                                    placeholder="NAME"
-                                    {...register
-                                        ("name", {
-                                            required:true,
-                                            maxLength:100,
-                                        }
-                                    )}
-                                />
-                                {errors.name && (
-                                    <p className="mt-1 text-primary-500">
-                                        {errors.name.type === "required" && "This field is required."}
-                                        {errors.name.type === "maxLength" && "Max length is 100 characters."}
-                                    </p>
-                                )}
-                                <input 
-                                    className={inputStyles}
-                                    type="text"
-                                    placeholder="EMAIL"
-                                    {...register
-                                        ("email", {
-                                            required:true,
-                                            pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                                        }
-                                    )}
-                                />
-                                {errors.email && (
-                                    <p className="mt-1 text-primary-500">
-                                        {errors.email.type === "required" && "This field is required."}
-                                        {errors.email.type === "email" && "Invalid email address."}
-                                    </p>
-                                )}
-                                <textarea 
-                                    className={inputStyles}
-                                    rows={4}
-                                    cols={50}
-                                    placeholder="MESSAGE"
-                                    {...register
-                                        ("message", {
-                                            required:true,
-                                            maxLength:2000,
-                                        }
-                                    )}
-                                />
-                                {errors.message && (
-                                    <p className="mt-1 text-primary-500">
-                                        {errors.message.type === "required" && "This field is required."}
-                                        {errors.message.type === "maxLength" && "Max length is 2000 characters."}
-                                    </p>
-                                )}
-                                <button
-                                    type="submit"
-                                    className="mt-5 rounded-lg bg-secondary-500 px-20 py-3 transition duration-500 hover:text-white"
-                                >
-                                    SUBMIT
-                                </button>
-                            </form>
-                        </motion.div>
-                        <motion.div
-                            className="relative mt-16 basis-1/2 md:mt-0"
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                            transition={{ delay: 0.2, duration: 0.5 }}
-                            variants={{
-                                hidden: { opacity: 0, y: 50 },
-                                visible: { opacity: 1, y: 0 },
-                            }}
-                        >
-                                <Image 
-                                    className="w-full rounded-lg"
-                                    alt="contact-us-page-graphic"
-                                    src={image10}
-                                />
-                        </motion.div>
-                    </div>
+                                SUBMIT
+                            </button>
+                        </form>
+                    </motion.div>
+                    <motion.div
+                        className="relative mt-16 basis-1/2 md:mt-0"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay: 0.2, duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, y: 50 },
+                            visible: { opacity: 1, y: 0 },
+                        }}
+                    >
+                        <Image 
+                            className="w-full rounded-lg"
+                            alt="contact-us-page-graphic"
+                            src={image10}
+                        />
+                    </motion.div>
+                </div>
             </motion.div>
         </section>
     )
--- app/components/Home/Navbar/Navbar.tsx
+++ app/components/Home/Navbar/Navbar.tsx
@@ -24,38 +24,6 @@ const Navbar = ({ openNav }: Props) => {
 
     return () => window.removeEventListener("scroll", handler);
   }, []);
-  
-
-  // useEffect(() => {
-  //   // Handle hash change and capture the current hash
-  //   const handleHashChange = () => {
-  //     setHash(window.location.hash);
-  //   };
-
-  //   // Initial set of hash
-  //   handleHashChange();
-
-  //   // Listen for hash changes
-  //   window.addEventListener("hashchange", handleHashChange);
-
-  //   // Cleanup listener
-  //   return () => {
-  //     window.removeEventListener("hashchange", handleHashChange);
-  //   };
-  // }, []);
-
-
-  // Function to handle the link click and update the URL
-  // const handleLinkClick = (url: string) => {
-  //   const targetHash = url.split("#")[1];
-    
-  //   // Update the hash in the URL
-  //   if (targetHash) {
-  //     window.location.hash = targetHash;  // This will change the URL without reloading the page
-  //   } else {
-  //     window.history.pushState({}, '', url);
-  //   }
-  // };
 
   return (
     <div className={`transition-all ${navBg ? "bg-[#284475] bg-opacity-95 shadow-md" : "fixed"} duration-200 h-[12vh] z-[100] fixed w-full`}>
@@ -71,18 +39,11 @@ const Navbar = ({ openNav }: Props) => {
         {/* NavLinks */}
         <div className="hidden lg:flex items-center space-x-10">
           {navLinks.map((link) => {
-            // Check if the current pathname and hash match the link's URL and hash
-            const isActive = pathname && hash === link.url;
-
             return (
               <Link
                 href={link.url}
                 key={link.id}
-                className={`text-white text-semibold hover:text-primary-300 transition-all duration-500 ${isActive ? 'text-primary-500' : 'text-secondary-500'}`}
-                // onClick={(e) => {
-                //   e.preventDefault();  // Prevent default link behavior
-                //   handleLinkClick(link.url);  // Update the URL programmatically
-                // }}
+                className={`text-white text-semibold hover:text-secondary-500 transition-all duration-500`}
               >
                 <p>{link.label}</p>
               </Link>
