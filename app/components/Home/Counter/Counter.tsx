824992850f7ca22ca6635c38ce9d0e4a002f0547
Sama-E <112719147+SdotKab@users.noreply.github.com>
2025-04-09 22:48:01 -0400

    counter text size

--- app/components/Home/Counter/Counter.tsx
+++ app/components/Home/Counter/Counter.tsx
@@ -5,24 +5,24 @@ import CountUp from 'react-countup'
 
 const Counter = () => {
   return (
-<section id="counter" className="mx-auto w-full flex items-center justify-center">
+<section id="counter" className="mx-auto w-full flex items-center justify-center p-3">
     <div className="flex flex-col mt-5 items-center justify-around gap-6 md:flex-row">
         <div className="mx-auto grid w-full grid-cols-3 gap-6 md:max-w-[800px] md:p-8">
             {/* First Column */}
             <div className="flex flex-col items-center justify-center p-8 space-y-4">
-                <p className="text-sm font-bold sm:text-lg md:text-3xl text-center">
+                <p className="text-md font-bold sm:text-xl md:text-3xl text-center">
                     <CountUp 
                         start={0}
                         end={4}
                         duration={2.75}
                     />
                 </p>
-                <h1 className="sm:text-md text-xs md:text-lg text-center">Boreholes</h1>
+                <h1 className="sm:text-md text-md md:text-lg text-center">Boreholes</h1>
             </div>
 
             {/* Second Column */}
             <div className="flex flex-col items-center justify-center p-8 space-y-4">
-                <p className="text-sm font-bold sm:text-lg md:text-3xl text-center">
+                <p className="text-md font-bold sm:text-xl md:text-3xl text-center">
                     <CountUp 
                         start={0}
                         end={54}
@@ -30,12 +30,12 @@ const Counter = () => {
                         duration={2.75}
                     />
                 </p>
-                <h1 className="sm:text-md text-xs md:text-lg text-center">Fundraisers</h1>
+                <h1 className="sm:text-md text-md md:text-lg text-center">Fundraisers</h1>
             </div>
 
             {/* Third Column */}
             <div className="flex flex-col items-center justify-center p-8 space-y-4">
-                <p className="text-sm font-bold sm:text-lg md:text-3xl text-center">
+                <p className="text-md font-bold sm:text-xl md:text-3xl text-center">
                     <CountUp 
                         start={0}
                         end={150000}
@@ -43,7 +43,7 @@ const Counter = () => {
                         duration={2.75}
                     />
                 </p>
-                <h1 className="sm:text-md text-xs md:text-lg text-center">Beneficiaries</h1>
+                <h1 className="sm:text-md text-md md:text-lg text-center">Beneficiaries</h1>
             </div>
         </div>
     </div>
